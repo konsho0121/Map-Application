575556a3f69215f2969d532f3b89ce08e6e7d436
Konsho0121 <trt.kon@icloud.com>
2019-03-23 17:59:05 +0900

    ThirdCommit

--- app/src/main/java/com/example/mapapplication/MapsActivity.java
+++ app/src/main/java/com/example/mapapplication/MapsActivity.java
@@ -5,6 +5,7 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -48,7 +49,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         LatLng sydney = new LatLng(40.82506283028448, 140.74014010221067);
         mMap.addMarker(new MarkerOptions().position(sydney).title("青森県庁"));
         LatLng aomori = new LatLng(53.82370253079868, 53.73743517537916);
-      //  mMap.addMarker(new MarkerOptions().position(aomori).title("どこか"));
+        //  mMap.addMarker(new MarkerOptions().position(aomori).title("どこか"));
         MarkerOptions options = new MarkerOptions();
         options.position(aomori);
         options.title("謎の場所");
@@ -72,7 +73,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
             }
         });
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Toast.makeText(MapsActivity.this, marker.getTitle() + "を押したね", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
+
 
 
